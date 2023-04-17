a7835b489b38e0343edb914d1b2499ac4af8279b
Fralou <fralou@protonmail.com>
2023-04-17 20:23:01 +0300

    feat: added solve

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text = '', key = '') {
+    text = text.trim().toUpperCase();
+    key = key.trim().toUpperCase();
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+    for (let i = 0, j = 0; i < text.length; i++) {
+      let letter = text[i];
+      if (/^[A-Z]$/.test(letter)) {
+        let shift = key[j % key.length].charCodeAt(0) - 65;
+        let encryptedLetter = String.fromCharCode((letter.charCodeAt(0) + shift - 65) % 26 + 65);
+        result += encryptedLetter;
+        j++;
+      } else {
+        result += letter;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(text = '', key = '') {
+    text = text.trim().toUpperCase();
+    key = key.trim().toUpperCase();
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+    for (let i = 0, j = 0; i < text.length; i++) {
+      let letter = text[i];
+      if (/^[A-Z]$/.test(letter)) {
+        let shift = key[j % key.length].charCodeAt(0) - 65;
+        let decryptedLetter = String.fromCharCode((letter.charCodeAt(0) - shift - 65 + 26) % 26 + 65);
+        result += decryptedLetter;
+        j++;
+      } else {
+        result += letter;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
+
+
 
 module.exports = {
   VigenereCipheringMachine
